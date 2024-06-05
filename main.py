41cd7f7bd24a4cbb73a144b9befaf6569ba6985f
lv1codeman <lv1codeman@gmail.com>
2024-06-05 17:18:46 +0800

    修改'Token驗證失敗'說明

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
 async def get_current_token(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="驗證失敗，無法取得Token",
+        detail="Token驗證失敗",
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
